2fe55d2862335b7ec43baf7eb8a0b8a160f337a4
yasmarques <yasmin.marquestito14@gmail.com>
2024-06-19 23:40:31 -0300

    api web sem segurança

---
+++ src/main/java/dio/web/api/controller/UsuarioController.java
@@ -0,0 +1,36 @@
+package dio.web.api.controller;
+
+import dio.web.api.repository.UsuarioRepository;
+import dio.web.api.model.Usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @PostMapping("/usuarios")
+    public void post(@RequestBody dio.web.api.model.Usuario usuario){
+        repository.save(usuario);
+    }
+    @PutMapping("/usuarios")
+    public void put(@RequestBody Usuario usuario){
+        repository.update(usuario);
+    }
+    @GetMapping("/usuarios")
+    public List<Usuario> getAll(){
+        return repository.listAll();
+    }
+    @GetMapping("/usuario/{id}")
+    public Usuario getOne(@PathVariable("id") Integer id){
+        return repository.finById(id);
+    }
+    @DeleteMapping("/usuarios/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        repository.remove(id);
+    }
+}
